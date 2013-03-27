8d39db7e5d5a7956da84b392fbb48d1c6e9d366e
Matt Burke <spraints@gmail.com>
2013-03-26 18:19:43 -0700

    Install your own dependencies.
    ... assuming ruby and bundler and cmake are available.

--- crawl.rb
+++ crawl.rb
@@ -1,9 +1,29 @@
 $: << File.expand_path('rugged/lib', File.dirname(__FILE__))
 
-require 'rugged'
+def main
+  require 'rugged'
 
-path = ARGV.first || '.'
-repo = Rugged::Repository.new(path)
-repo.each_id do |oid|
-  puts repo.read(oid).len
+  path = ARGV.first || '.'
+  repo = Rugged::Repository.new(path)
+  repo.each_id do |oid|
+    puts repo.read(oid).len
+  end
+end
+
+
+begin
+  main
+rescue LoadError
+  cmd = 'git submodule update --init --recursive'
+  puts "$ #{cmd}" ; system cmd
+
+  Dir.chdir 'rugged' do
+    cmd = 'bundle install --path .bundle/gems'
+    puts "$ #{cmd}" ; system cmd
+
+    cmd = 'bundle exec rake compile'
+    puts "$ #{cmd}" ; system cmd
+  end
+
+  main
 end
